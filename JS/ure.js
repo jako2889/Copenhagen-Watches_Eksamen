097bc47f86469ccb18dc482034dc529a3e019c16
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2018-12-05 17:24:15 +0100

    1. Script i ure. uden filter + Oprettet Single page index, css og html

---
+++ JS/ure.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", getJson);
+
+//GLOBALE VARIABLER -------------------------
+
+let allPost;
+
+let postTarget = document.querySelector(".product_template");
+
+let postOutput = document.querySelector(".product_wrapper");
+
+
+//Hentf ---------------------------------------------------------------------------------------------------------------------------------------------------------
+async function getJson() {
+
+    let jsonObject = await fetch("https://magnusvjensen.dk/2_semester/eksamen/wordpress/wp-json/wp/v2/product");
+
+    allPost = await jsonObject.json();
+
+
+    visPost();
+}
+
+//visPost ---------------------
+function visPost() {
+
+    //Gennemgår alle posts
+    allPost.forEach(post => {
+   console.log("FEST");
+
+        udSkrivPost(post);
+
+        //Udskriver fra template til dokument
+        function udSkrivPost(post) {
+
+            //Kloner template elementerne
+            let klon = postTarget.cloneNode(true).content;
+
+            klon.querySelector(".prod_image").src = post.acf.image_prime.url;
+
+            klon.querySelector(".prod_title").textContent = post.acf.title;
+            klon.querySelector(".prod_kollektion").textContent = post.acf.kollektion;
+            klon.querySelector(".prod_dial").textContent = post.acf.dial;
+            klon.querySelector(".prod_rem").textContent = post.acf.rem;
+            klon.querySelector(".prod_pris").textContent = post.acf.pris + "kr.";
+
+
+            console.log(post.acf.pris);
+            //Udskriver
+            postOutput.appendChild(klon);
+
+        }
+
+
+    })
+}
