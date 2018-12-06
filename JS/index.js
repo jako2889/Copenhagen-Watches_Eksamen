eff7182b9475f08a02866102a9fde5ff8f39e12e
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2018-12-06 12:40:14 +0100

    product slider

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,80 @@
-//PROMOTION SLIDER ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+document.addEventListener("DOMContentLoaded", getJson);
+
+//GLOBALE VARIABLER -------------------------
+
+
+let postTarget = document.querySelector(".promotion_product_template");
+
+let postOutput = document.querySelector("#hest");
+
+
+let allPost;
+
+
+//Hentf ---------------------------------------------------------------------------------------------------------------------------------------------------------
+async function getJson() {
+
+    let jsonObject = await fetch("https://magnusvjensen.dk/2_semester/eksamen/wordpress/wp-json/wp/v2/product/?per_page=5");
+
+    allPost = await jsonObject.json();
+
+
+
+    visPost();
+
+}
+
+
+
+
+//visPost ---------------------
+function visPost() {
+
+    //Gennemgår alle posts
+    allPost.forEach(post => {
+
+
+        udSkrivPost(post);
+        //Udskriver fra template til dokument
+        function udSkrivPost(post) {
+
+            //Kloner template elementerne
+            let klon = postTarget.cloneNode(true).content;
+
+            klon.querySelector(".prom_prod_image").src = post.acf.image_prime.url;
+
+            klon.querySelector(".prom_prod_title").textContent = post.acf.title;
+            klon.querySelector(".prom_prod_kollektion").textContent = post.acf.kollektion;
+            klon.querySelector(".prom_prod_dial").textContent = post.acf.dial;
+            klon.querySelector(".prom_prod_rem").textContent = post.acf.rem;
+            klon.querySelector(".prom_prod_pris").textContent = post.acf.pris + " " + "kr";
+
+
+
+            klon.querySelector(".mySlides").addEventListener("click",() =>{
+
+                 window.location.href = "ure_single_page.html?id=" + post.id;
+
+
+            });
+
+
+            console.log(post.acf.image_prime.url);
+            //Udskriver
+            postOutput.appendChild(klon);
+
+            showDivs();
+
+        }
+
+
+    })
+}
+
+
+
+//PROMOTION SLIDER ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -26,5 +101,27 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " w3-red";
 }
-
 //PROMOTION SLIDER END ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- JS/ure.js
+++ JS/ure.js
@@ -31,7 +31,7 @@ async function getJson() {
 }
 
 
-function filter(){
+function filter() {
 
 
 
