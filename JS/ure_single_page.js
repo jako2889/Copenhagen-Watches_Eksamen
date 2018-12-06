3bc005c214f783b85b9c2d62b2a9efbc8f02ebf3
Magnus Vagn Jensen <magnusvagnjensen@Magnuss-MBP-3.domain_not_set.invalid>
2018-12-06 15:08:15 +0100

    single page spec,s osv

--- JS/ure_single_page.js
+++ JS/ure_single_page.js
@@ -22,18 +22,20 @@ async function getJson() {
     function visProduct() {
 
 
-            let display = document.querySelector(".single_product_container");
+            let display = document.querySelector(".site_content_wrapper");
 
         allPost.forEach(post => {
                 if (post.id == sPost) {
 
                  console.log("HESTE FEST")
 
+            //HEADER
             display.querySelector(".head_title").textContent = post.acf.title;
             display.querySelector(".head_kollektion").textContent = post.acf.kollektion;
             display.querySelector(".head_dial").textContent = post.acf.dial;
 
 
+            //MAIN IMAGE
             display.querySelector(".image_1").src = post.acf.image_prime.url;
             display.querySelector(".image_2").src = post.acf.image_2.url;
             display.querySelector(".image_3").src = post.acf.image_3.url;
@@ -42,7 +44,7 @@ async function getJson() {
             display.querySelector(".image_6").src = post.acf.image_6.url;
             display.querySelector(".image_7").src = post.acf.image_7.url;
 
-
+            //IMAGE KNAPPER
             display.querySelector(".but_1").src = post.acf.image_prime.url;
             display.querySelector(".but_2").src = post.acf.image_2.url;
             display.querySelector(".but_3").src = post.acf.image_3.url;
@@ -51,7 +53,26 @@ async function getJson() {
             display.querySelector(".but_6").src = post.acf.image_6.url;
             display.querySelector(".but_7").src = post.acf.image_7.url;
 
+            //CTA KOB KNAP
+            display.querySelector(".cta_dial").textContent = post.acf.dial;
+            display.querySelector(".cta_pris").textContent = post.acf.pris + " " + "kr";
+            display.querySelector(".cta_rem").textContent = post.acf.rem;
 
+            //BEKSRIVELSE
+            display.querySelector(".bskr_text").innerHTML = post.content.rendered;
+
+            //SPEC
+            display.querySelector(".s_1").textContent = post.acf.varenummer;
+            display.querySelector(".s_2").textContent = post.acf.tykkelse;
+            display.querySelector(".s_3").textContent = post.acf.case;
+            display.querySelector(".s_4").textContent = post.acf.urskive_farve;
+            display.querySelector(".s_5").textContent = post.acf.urets_viser;
+            display.querySelector(".s_6").textContent = post.acf.rem;
+            display.querySelector(".s_7").textContent = post.acf.vandafvisende;
+
+
+
+              console.log(post.acf.dial);
 
 
 
